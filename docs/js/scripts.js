c4cd59690117d8db94a831452c22b78dc96c21d7
aikovalenko <ai.kovalenko@mail.ru>
2017-11-20 22:56:25 +0300

    first push

---
+++ docs/js/scripts.js
@@ -0,0 +1,123 @@
+// $('.js-cycle-slider').cycle({
+//     fx:       'fade',
+//     speed:    1,
+//     timeout:  5000,
+//     paused:    false,
+//     slides: '>',
+//     autoHeight: 'calc',
+//     swipe: false
+// }).on( 'cycle-before', function( event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag ) {
+//     console.log('pause');
+//
+//     $('.js-cycle-slider').addClass('fade');
+//     setTimeout( function () {
+//         $('.js-cycle-slider').cycle('pause');
+//     }, 1000);
+//     setTimeout( function () {
+//         $('.js-cycle-slider').removeClass('fade').cycle('resume');
+//     }, 2000);
+// });
+
+
+$('.js-cycle-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dors: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    fade: true,
+    infinite: true,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3000
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+
+    $('.js-cycle-slider').addClass('fade');
+    setTimeout( function () {
+        $('.js-cycle-slider').removeClass('fade');
+        $('.slick-current .tlt').textillate();
+    }, 1000);
+
+}).on('afterChange', function(event, slick, currentSlide){
+
+
+});
+$('.slick-current .tlt').textillate();
+// ret();
+
+// function ret() {
+//     $('.slick-current .tlt').textillate({
+//         // the default selector to use when detecting multiple texts to animate
+//         // selector: '.texts',
+//
+//         // enable looping
+//         loop: false,
+//
+//         // sets the minimum display time for each text before it is replaced
+//         minDisplayTime: 2000,
+//
+//         // sets the initial delay before starting the animation
+//         // (note that depending on the in effect you may need to manually apply
+//         // visibility: hidden to the element before running this plugin)
+//         initialDelay: 0,
+//
+//         // set whether or not to automatically start animating
+//         autoStart: true,
+//
+//         // custom set of 'in' effects. This effects whether or not the
+//         // character is shown/hidden before or after an animation
+//         inEffects: [],
+//
+//         // custom set of 'out' effects
+//         outEffects: [ 'hinge' ],
+//
+//         // in animation settings
+//         in: {
+//             // set the effect name
+//             effect: 'fadeInLeftBig',
+//
+//             // set the delay factor applied to each consecutive character
+//             delayScale: 1.5,
+//
+//             // set the delay between each character
+//             delay: 50,
+//
+//             // set to true to animate all the characters at the same time
+//             sync: false,
+//
+//             // randomize the character sequence
+//             // (note that shuffle doesn't make sense with sync = true)
+//             shuffle: false,
+//
+//             // reverse the character sequence
+//             // (note that reverse doesn't make sense with sync = true)
+//             reverse: false,
+//
+//             // callback that executes once the animation has finished
+//             callback: function () {}
+//         },
+//
+//         // out animation settings.
+//         out: {
+//             effect: 'hinge',
+//             delayScale: 1.5,
+//             delay: 0,
+//             sync: false,
+//             shuffle: false,
+//             reverse: false,
+//             callback: function () {}
+//         },
+//
+//         // callback that executes once textillate has finished
+//         callback: function () {
+//             var t = $('.tlt').children('span').attr('aria-label');
+//             $('.tlt').html(t);
+//             $('.tlt').children('span').remove();
+//             $('.tlt').children('ul').remove();
+//         },
+//
+//         // set the type of token to animate (available types: 'char' and 'word')
+//         type: 'char'
+//     });
+// }
---
+++ src/js/libs.js
@@ -0,0 +1,5 @@
+//= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/jquery-cycle-2/src/jquery.cycle.all.js
+//= ../../node_modules/slick-carousel/slick/slick.min.js
+//= ../../node_modules/lettering.js/index.js
+//= ../libs/jquery.textillate.js
